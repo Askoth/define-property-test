baab0786966159e091b5a250ed267b75d218086d
Marcelo Oliveira <askoth@gmail.com>
2019-01-17 22:08:09 +0100

    Killing dom-utils, not much sense in have this as another module. adding colors to labels

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import Store from './store.js';
 import Table from './table.js';
-import { getScoreValues, bindInputs } from './dom-utils.js';
 import ajax from './ajax.js';
 
 ajax.fetchJson({
@@ -34,3 +33,34 @@ ajax.fetchJson({
 }).catch((err) => {
     throw err;
 });
+
+function getScoreValues(checkBoxes) {
+    return Array.from(checkBoxes).reduce((prev, el) => {
+        prev[el.value] = el.checked;
+
+        return prev
+    }, {});
+}
+
+function bindInputs ({ checkBoxes, inputText, store }) {
+    Array.from(checkBoxes).forEach((el) => {
+        el.addEventListener('change', (e) => {
+            let scores = store.data.scores;
+
+            scores[e.target.value] = e.target.checked;
+
+            // forces defineProperty set
+            store.data.scores = scores;
+        })
+    });
+
+
+    inputText.addEventListener('keyup', (e) => {
+        let text = store.data.text;
+
+        if (text != e.target.value) {
+            store.data.text = e.target.value;
+        };
+    })
+
+}
